7bc05dc7353d83795e62f8a9e53026d161d6a4d6
Jonathan Brodsky <jonbro@users.noreply.github.com>
2019-12-03 20:12:57 -0500

    brought over the finished tutorial game

---
+++ examples/skiiifree.ts
@@ -0,0 +1,130 @@
+// this line puts the mglue classes inside the m variable.
+// you might see code that moves the classes directly into the
+// namespace of your file, but those are just shorthand and not necessary
+let m = mglue
+
+class Player extends m.Actor
+{
+    // this function is called when the player is ititialized
+    begin()
+    {
+        // make the player be represented by a small square
+        this.drawing
+            .addRect(0.05);
+
+        // put the player in the top middle of the screen
+        this.setPosition(new m.Vector(0.5, 0.25));
+    }
+    // this function is called every frame
+    update()
+    {
+        let steeringSpeed = 0.01;
+        // may as well support keyboard, mouse and touchscreen, no reason not to :)
+        if((m.Mouse.isPressed && m.Mouse.position.x < 0.5) || m.Keyboard.keyDown[m.Keyboard.LEFT])
+        {
+            // mglue adds a few extension methods to numbers
+            // which are helpful for simple game stuff.
+            // clamp can take a min and max value, or by default clamps to 0-1
+            this.position.x = (this.position.x-steeringSpeed).clamp();
+        }
+        else if((m.Mouse.isPressed && m.Mouse.position.x >= 0.5) || m.Keyboard.keyDown[m.Keyboard.RIGHT])
+        {
+            this.position.x = (this.position.x+steeringSpeed).clamp();
+        }
+   }
+}
+
+// in our game, trees will be the enemy, so add a class for them
+class TreeEnemy extends m.Actor
+{
+    begin()
+    {
+        // the tree will be represented by a green rectangle
+        this.drawing
+            .setColor(m.Color.green)
+            .addRect(0.02, 0.04);
+        // it will start at a random position at the bottom of the screen
+        // and move upwards
+        this.setPosition(new m.Vector(m.Random.value(), 1));
+        this.setVelocity(new m.Vector(0, -0.01));
+    }
+    update()
+    {
+        // if this and the player overlap
+        this.checkOverlap(Player, (p:any)=>{
+            // kill this tree
+            this.destroy();
+            // kill the player
+            p.destroy();
+            // end the game
+            g.endGame();
+        });
+        // finally, if this tree goes off the screen, destroy it.
+        if(this.position.y < 0)
+        {
+            this.destroy();
+        }
+    }
+}
+
+class ScoreThing extends m.Actor
+{
+    begin()
+    {
+        // the tree will be represented by a green rectangle
+        this.drawing
+            .setColor(m.Color.yellow)
+            .addRect(0.03); 
+        this.setPosition(new m.Vector(m.Random.value(), 1));
+        this.setVelocity(new m.Vector(0, -0.01));
+    }
+    update()
+    {
+        // if this and the player overlap
+        this.checkOverlap(Player, (p:any)=>{
+            // remove this score thing
+            this.destroy();
+            // raise the game score by one
+            g.score++;
+            // pop out some particle effects
+            let particles = new m.ParticleSystem();
+		    particles.position.set(this.position);
+            particles.color = m.Color.yellow;
+		    particles.count = 10;
+        });
+        // this should be familiar from the tree code
+        if(this.position.y < 0)
+        {
+            this.destroy();
+        }
+    }
+}
+
+// our game class, handles adding the player and enemies
+class BasicGame extends m.Game
+{
+    onBeginGame()
+    {
+      new Player();
+      // add a new tree every 20 frames
+      this.newFiber().do(()=>{new TreeEnemy()}).wait(20);
+      // add a new score thing every 40 frames
+      this.newFiber().do(()=>{new ScoreThing()}).wait(40);
+    }
+}
+
+// declare a variable that will hold your game
+var g : BasicGame;
+
+// before we start the game, setup your configuration.
+// configure the game name
+m.Config.title = "FREELY SKIII";
+// configure the save file name
+m.Config.saveName = "freelySkiily";
+
+// this function is called when the webpage is up and running.
+// the callback provided creates an instance of the basic game and stores it in the variable declared earlier.
+m.Game.runOnReady(()=>{
+    g = new BasicGame();
+  	g.enableLeaderboard(getLeaderboardURL());
+});
